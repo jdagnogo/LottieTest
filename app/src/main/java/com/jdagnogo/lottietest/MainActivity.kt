5cc6bf08e7a8f436008dd5971e1ef0d2a4e58038
Jeff <jf.dagnogo@exus.co.uk>
2020-01-20 16:33:50 +0200

    add like button

--- app/src/main/java/com/jdagnogo/lottietest/MainActivity.kt
+++ app/src/main/java/com/jdagnogo/lottietest/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.jdagnogo.lottietest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        thumb_up.setOnClickListener {
+                with(thumb_down) {
+                    progress = 0f
+                    pauseAnimation()
+                }
+                thumb_up.playAnimation()
+                Toast.makeText(this@MainActivity, "Cheers!!", Toast.LENGTH_SHORT).show()
+        }
+
+        thumb_down.setOnClickListener {
+            with(thumb_up) {
+               setProgress(0f)
+               pauseAnimation()
+            }
+            thumb_down.playAnimation()
+            Toast.makeText(this@MainActivity, "Boo!!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
